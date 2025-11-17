9fb6ad16b4004ca9bba121e93a3c3bfd7ff12f78
Humberto Oscata <loscataf@bsg.com>
2025-11-17 10:01:23 -0500

    feat: modal chat messenger y carga de historial mensajes

--- src/app/integra/areas/marketing/campania-facebook/messenger-facebook-chat/messenger-facebook-chat-modal/messenger-facebook-chat-modal.component.ts
+++ src/app/integra/areas/marketing/campania-facebook/messenger-facebook-chat/messenger-facebook-chat-modal/messenger-facebook-chat-modal.component.ts
@@ -6,21 +6,27 @@ import {
   OnInit,
   OnChanges,
   SimpleChanges,
+  AfterViewChecked,
+  ElementRef,
+  ViewChild,
 } from '@angular/core';
 import { constApiMarketing } from '@environments/constApi';
 import { ChatMessengerFacebook } from '@marketing/models/interfaces/messenger-facebook-chat';
 import { AlertaService } from '@shared/services/alerta.service';
 import { IntegraService } from '@shared/services/integra.service';
-
 @Component({
   selector: 'app-messenger-facebook-chat-modal',
   templateUrl: './messenger-facebook-chat-modal.component.html',
   styleUrls: ['./messenger-facebook-chat-modal.component.scss'],
 })
-export class MessengerFacebookChatModalComponent implements OnInit {
+export class MessengerFacebookChatModalComponent
+  implements OnInit, OnChanges, AfterViewChecked
+{
   @Input() identificadorAmbitoPagina: string;
   @Output() close = new EventEmitter<void>();
-
+  @ViewChild('chatHistory') chatHistoryRef: ElementRef;
+  private shouldScrollChat = false;
+  loaderReloadChat: boolean = false;
   historialChats: ChatMessengerFacebook[] = [];
   newMessage: string = '';
   panelOpenIndex: number | null = null;
@@ -40,6 +46,15 @@ export class MessengerFacebookChatModalComponent implements OnInit {
     private _alertaService: AlertaService,
     private integraService: IntegraService
   ) {}
+
+  ngOnChanges(changes: SimpleChanges): void {
+    if (
+      changes['identificadorAmbitoPagina'] &&
+      this.identificadorAmbitoPagina
+    ) {
+      this.obtenerHistorialChatPorPSId(this.identificadorAmbitoPagina);
+    }
+  }
 
   ngOnInit(): void {
     // Mock de oportunidades
@@ -65,14 +80,15 @@ export class MessengerFacebookChatModalComponent implements OnInit {
     ];
   }
 
-  // ngOnChanges(changes: SimpleChanges): void {
-  //   if (
-  //     changes['identificadorAmbitoPagina'] &&
-  //     this.identificadorAmbitoPagina
-  //   ) {
-  //     this.obtenerHistorialChatPorPSId(this.identificadorAmbitoPagina);
-  //   }
-  // }
+  ngAfterViewChecked(): void {
+    if (this.shouldScrollChat && this.chatHistoryRef) {
+      try {
+        this.chatHistoryRef.nativeElement.scrollTop =
+          this.chatHistoryRef.nativeElement.scrollHeight;
+      } catch {}
+      this.shouldScrollChat = false;
+    }
+  }
 
   obtenerHistorialChatPorPSId(identificadorAmbitoPagina: string) {
     if (!identificadorAmbitoPagina) return;
@@ -83,6 +99,7 @@ export class MessengerFacebookChatModalComponent implements OnInit {
       .subscribe({
         next: (data: any) => {
           this.historialChats = data.body as ChatMessengerFacebook[];
+          this.shouldScrollChat = true;
         },
         error: (err) => {
           this._alertaService.notificationError('Error al buscar chat por ID');
@@ -91,7 +108,25 @@ export class MessengerFacebookChatModalComponent implements OnInit {
       });
   }
 
-  // Expansion panel
+  reloadChats() {
+    this.loaderReloadChat = true;
+    this.integraService
+      .postJsonResponse(`${constApiMarketing.ObtenerHistorialChatPorPSID}`, {
+        identificadorAmbitoPagina: this.identificadorAmbitoPagina,
+      })
+      .subscribe({
+        next: (data: any) => {
+          this.historialChats = data.body as ChatMessengerFacebook[];
+          this.loaderReloadChat = false;
+        },
+        error: (err) => {
+          this._alertaService.notificationError('Error al buscar chat por ID');
+          this.loaderReloadChat = false;
+        },
+      });
+  }
+
+  // Expansion panel Oportunidad
   onPanelExpanded(expanded: boolean, idx: number) {
     if (expanded) {
       this.panelOpenIndex = idx;
