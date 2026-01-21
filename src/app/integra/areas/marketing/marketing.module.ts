c0c3919fdec5b1c90706bfde2bccc0772603ed4e
DESKTOP-ISGJLTO\admin <carlos_h_c_r@hotmail.com>
2026-01-21 11:31:14 -0500

    fix: categoriaargumento

--- src/app/integra/areas/marketing/marketing.module.ts
+++ src/app/integra/areas/marketing/marketing.module.ts
@@ -174,6 +174,15 @@ import { FacebookLeadsRecuperacionDatosService } from '@shared/services/facebook
 import { MessengerFacebookChatComponent } from './campania-facebook/messenger-facebook-chat/messenger-facebook-chat.component';
 import { MessengerFacebookChatGrillaComponent } from './campania-facebook/messenger-facebook-chat/messenger-facebook-chat-grilla/messenger-facebook-chat-grilla.component';
 import { MessengerFacebookChatModalComponent } from './campania-facebook/messenger-facebook-chat/messenger-facebook-chat-modal/messenger-facebook-chat-modal.component';
+//import { CampaniaRemarketingGeneralComponent } from './campanias-mailing/campania-remarketing-general/campania-remarketing-general.component';
+//import { GraficaRendimientoComponent } from './campanias-mailing/campania-remarketing-general/grafica-rendimiento/grafica-rendimiento.component';
+//import { CrearEditarCampaniaComponent } from './campanias-mailing/campania-remarketing-general/crear-editar-campania/crear-editar-campania.component';
+//import { DetalleCampaniaComponent } from './campanias-mailing/campania-remarketing-general/detalle-campania/detalle-campania.component';
+import { CategoriaArgumentosComponent } from './configuraciones/categoria-argumentos/categoria-argumentos.component';
+import { CategoriasComponent } from './configuraciones/categoria-argumentos/categorias/categorias.component';
+import { ProgramasComponent } from './configuraciones/categoria-argumentos/programas/programas.component';
+import { ModalEditarProgramaComponent } from './configuraciones/categoria-argumentos/programas/modal-editar-programa/modal-editar-programa.component';
+import { ModalCrearProgramaComponent } from './configuraciones/categoria-argumentos/programas/modal-crear-programa/modal-crear-programa.component';
 
 
 @NgModule({
@@ -348,7 +357,16 @@ import { MessengerFacebookChatModalComponent } from './campania-facebook/messeng
     ModalDetalleLeadFacebookComponent,
     MessengerFacebookChatComponent,
     MessengerFacebookChatGrillaComponent,
-    MessengerFacebookChatModalComponent
+    MessengerFacebookChatModalComponent,
+    //CampaniaRemarketingGeneralComponent,
+    //GraficaRendimientoComponent,
+    //CrearEditarCampaniaComponent,
+    //DetalleCampaniaComponent,
+    CategoriaArgumentosComponent,
+    CategoriasComponent,
+    ProgramasComponent,
+    ModalEditarProgramaComponent,
+    ModalCrearProgramaComponent
   ],
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
   imports: [
---
+++ src/app/integra/areas/marketing/models/interfaces/categoria-argumentos.ts
@@ -0,0 +1,36 @@
+export interface ProgramaConfigurado {
+  id: number;
+  nombre: string;
+  usuarioModificacion: string;
+  fechaModificacion: Date;
+}
+
+export interface ProgramaGeneral {
+  id: number;
+  nombre: string;
+}
+
+export interface CategoriaArgumento {
+  id: number;
+  nombre: string;
+  usuarioModificacion: string;
+  fechaModificacion: Date;
+}
+
+export interface ProgramaConfiguradoDetalle {
+  id: number;
+  idProgramaGeneral: number;
+  nombrePrograma: string;
+  categoriasArgumento: CategoriaArgumentoPorPrograma[];
+}
+export interface CategoriaArgumentoPorPrograma {
+  id: number;
+  nombre: string;
+  argumentos: Argumento[];
+}
+export interface Argumento {
+  id: number;
+  nombre: string;
+  descripcion: string;
+  prioridad: number;
+}
