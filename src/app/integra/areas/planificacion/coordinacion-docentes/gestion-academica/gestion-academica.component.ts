86841c5d787dea0434f9c5d5efdbf3cffef15f18
Marcoxjocx02 <mvillanuevat@bsginstitute.com>
2026-03-05 19:16:59 -0500

    Feat:Creacion de Modulo base

---
+++ src/app/integra/areas/planificacion/coordinacion-docentes/gestion-academica/gestion-academica.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-gestion-academica',
+  templateUrl: './gestion-academica.component.html',
+  styleUrls: ['./gestion-academica.component.scss']
+})
+export class GestionAcademicaComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
